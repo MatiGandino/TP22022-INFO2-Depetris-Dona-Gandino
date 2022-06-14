5fc1a80ba85fdad85131873b7bc9f297b64d72cb
Rafael-Dona <69308972+Rafael-Dona@users.noreply.github.com>
2022-06-14 20:53:09 -0300

    Agrego de comentarios

--- Lectura_de_los_datos_en_computadora/Lectura_de_los_datos_en_computadora.c
+++ Lectura_de_los_datos_en_computadora/Lectura_de_los_datos_en_computadora.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdint.h> //para utilizar uint16_t y uint32_t
-#define c 24
+#include <stdint.h> //Para utilizar uint16_t y uint32_t.
+#define c 24	    //Declaracion de "c" para utilizar en el for.
 
-typedef struct mediciones {
+typedef struct mediciones { 			//Estructura que almacena los valores leídos.
 	struct temperatura {
-		uint32_t tiempotemp;
-		uint16_t valortemp;
-		char indictemp;
+		uint32_t tiempotemp;		//Variable Tiempo.
+		uint16_t valortemp;		//Variable Valor del sensor.
+		char indictemp;			//Variable de la letra del sensor.
 		char temp;
 	}temperatura;
 	struct humedad {
@@ -29,20 +29,20 @@ int main() {
 	
 	FILE *fpt;
 	
-	fpt = fopen("D:/Rafael/Desktop/ARCHIVO.dat", "r");      //Agregar la direccion del archivo.
+	fpt = fopen("D:/Rafael/Desktop/ARCHIVO.dat", "r");      //Lectura del archivo.
 	
 	if(fpt == NULL){
-		printf("\n  Error al abrir el archivo...");
+		printf("\n  Error al abrir el archivo...");	//Si hay algún problema, mensiona que hay un problema con la lectura.
 		return -1;
 	}
 	
 	printf("Apertura del archivo exitosa...\n\n");
 	
-	fread(med, sizeof(mediciones), c, fpt);
+	fread(med, sizeof(mediciones), c, fpt); 		//Lectura de todos los valores del archivo.
 	fclose (fpt);
 	
 
-	for (int i = 0; i < c; i++){
+	for (int i = 0; i < c; i++){				//For para escribir los datos leídos.
 		printf ("Hora %d:\n", i+1);
 		printf ("  Sensor: Temperatura(%c). \n  Valor: %d°C.\n  Tiempo desde la ultima medicion: %d ms.\n\n", med[i].temperatura.indictemp, med[i].temperatura.valortemp, med[i].temperatura.tiempotemp);
 		printf ("  Sensor: Humedad(%c). \n  Valor: %d÷.\n  Tiempo desde la ultima medicion: %d ms.\n\n", med[i].humedad.indichum, med[i].humedad.valorhum, med[i].humedad.tiempohum)  ;
